e0b061f41800451388380804360a7156d8523636
lrr68 <lucca@luccaaugusto.xyz>
2021-09-21 18:30:03 -0300

    improve game loop

--- snake/main.c
+++ snake/main.c
@@ -1,9 +1,13 @@
+#define _XOPEN_SOURCE 500
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "./pixel.h"
 #include "./board.h"
 #include "./snake.h"
+
+#define FPS 6
 
 int
 should_end_game(Snake *s, Board *b)
@@ -15,17 +19,51 @@ should_end_game(Snake *s, Board *b)
 	return 0;
 }
 
+void
+clear_screen()
+{
+	printf("\x1b[%dD", WIDTH);
+	printf("\x1b[%dA", HEIGHT);
+}
+
+void
+show_cursor(int show)
+{
+  if (show)
+    fputs("\x1b[?25h", stdout);
+  else
+    fputs("\x1b[?25l", stdout);
+}
+void
+end_game()
+{
+	printf("\x1b[%dB", WIDTH);
+	printf("\x1b[%dC", HEIGHT);
+	printf("GAME OVER\n");
+}
+
 int main()
 {
-	Snake *snake = new_snake(1);
 	Board *board = new_board(HEIGHT, WIDTH);
+	Snake *snake = new_snake(board);
+	Direction dir = rand() % COUNT_DIR;
+	show_cursor(0);
+	place_snake(snake, board);
 
 	do {
-		place_snake(snake, board);
-		board_drop_apple(board);
+		if (!board_get_has_apple(board))
+			board_drop_apple(board);
+
 		show_board(board);
 
+		clear_screen();
+		move_snake(snake, board, dir);
+		usleep(1000 * 1000 / FPS);
 	} while (!should_end_game(snake, board));
+
+	destroy_board(board);
+	end_game();
+	show_cursor(1);
 
 	return 0;
 }
